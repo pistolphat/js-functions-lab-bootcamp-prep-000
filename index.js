20e562f6ea5710487f6f21b114b7fb401e053aa8
Phat Doan <phatdoan@yahoo.com>
2018-08-03 00:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holidayname, days) {
-  return "It's 20 days until Mother's Day!"
+  return `It's ${days} days until ${holiday}!`
 }
