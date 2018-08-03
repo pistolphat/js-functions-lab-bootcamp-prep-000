b9e02eb2ba80675897d1d799df66ee31b867c8d4
Phat Doan <phatdoan@yahoo.com>
2018-08-03 00:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holidayname, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
